01bd7662e8ffd651b8920fde5f721e04a0c5768b
andriyka <andriyka@playtech.com>
2017-01-12 14:15:37 +0000

    remove web boot from tests

--- benchmark/src/test/java/io/openexchange/BenchmarkApplicationTest.java
+++ benchmark/src/test/java/io/openexchange/BenchmarkApplicationTest.java
@@ -2,18 +2,14 @@ package io.openexchange;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootApplication
 @TestPropertySource(locations = "classpath:test.properties")
 public class BenchmarkApplicationTest {
-    @Value("${local.server.port}")
-    private int port;
-
     @Test
     public void testBoot() throws Exception {
     }
